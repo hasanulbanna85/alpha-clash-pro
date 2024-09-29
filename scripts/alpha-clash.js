b2235d7b256a6771912e227966fb144fa7f53094
hasanulbanna85 <hasanulbanna265@gmail.com>
2024-09-29 19:56:00 +0600

    play again

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,7 +1,30 @@
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log('random alphabet', expectedAlphabet);
+
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+        removeBackgroundColorById(expectedAlphabet);
+        console.log('..................................');
+        continueGame();
+    }
+    else{
+        console.log('you missed. you lost a life');
+        console.log('..................................');
+    }
+}
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
 function continueGame(){
     const alphabet = getARandomAlphabet();
 
-    const currentAlphabetElement = document.getElementById(current-alphabet);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
 
     setBackgroundColorById(alphabet);
