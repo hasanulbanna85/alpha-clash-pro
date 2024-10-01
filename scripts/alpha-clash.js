6e17cdde46ea866ac8e6eb20b314b3fc68768c60
hasanulbanna85 <hasanulbanna265@gmail.com>
2024-10-02 03:54:06 +0600

    score update and end button create

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,6 +1,11 @@
 function handleKeyboardKeyUpEvent(event){
     const playerPressed = event.key;
 
+    // stop the game if pressed 'Esc'
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
@@ -8,22 +13,19 @@ function handleKeyboardKeyUpEvent(event){
     if(playerPressed === expectedAlphabet){
         removeBackgroundColorById(expectedAlphabet);
         
-        const currentScore = getTextElementById('current-score');
+        const currentScore = getTextElementValueById('current-score');
         const updatedScore = currentScore + 1;
-        setTextElementById('current-score', updatedScore);
+        setTextElementValueById('current-score', updatedScore);
         continueGame();
     }
     else{
-        const currentLife = getTextElementById('current-life');
+        const currentLife = getTextElementValueById('current-life');
         const updatedLife = currentLife - 1;
-        setTextElementById('current-life', updatedLife);
+        setTextElementValueById('current-life', updatedLife);
 
-        /* const currentLifeElement = document.getElementById('current-life');
-        const currentLifeText = currentLifeElement.innerText;
-        const currentLife = parseInt(currentLifeText);
-        const newLife = currentLife - 1;
- */
-        // currentLifeElement.innerText = newLife;
+        if(updatedLife === 0){
+            gameOver();
+        }
     }
 }
 
@@ -31,15 +33,34 @@ document.addEventListener('keyup', handleKeyboardKeyUpEvent);
 
 function continueGame(){
     const alphabet = getARandomAlphabet();
-
+    
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
-
+    
     setBackgroundColorById(alphabet);
 }
 
 function play(){
+    // hide everythig, show only playground
     hideElementById('home-screen');
+    hideElementById('final-score');
     showElementById('play-ground');
+    
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+    
     continueGame();
+    
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    const lastScore = getTextElementValueById('current-score');
+    setTextElementValueById('last-score', lastScore);
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBackgroundColorById(currentAlphabet);
 }
